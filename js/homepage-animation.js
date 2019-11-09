8ea02d592d37312d042224c9e719fb3e6466b49f
jacques <jacques.g.elliott@gmail.com>
2019-11-09 23:31:50 +0000

    animations and links

---
+++ js/homepage-animation.js
@@ -0,0 +1,25 @@
+var transitionTime = 2000;
+var animationFps = 60;
+
+function fadeChildrenOf(element) {
+  var children = element.children;
+  for (var i = 0; i < children.length; i++) {
+    fadeAway(children[i]);
+  }
+}
+
+function fadeAway(element) {
+  element.style.opacity = 1;
+
+  var timeBetweenDecrements = transitionTime / animationFps;
+  var opacityDecrements = 1 / timeBetweenDecrements;
+
+  var fade = setInterval(function() {
+    if (element.style.opacity > 0) {
+      element.style.opacity -= 0.025;
+    } else {
+      clearInterval(fade);
+      element.remove();
+    }
+  }, timeBetweenDecrements);
+}
--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,15 @@
-function onPlayClick() {
-  var element = document.getElementById("play-button"); 
+function onPlayClick(playButtonElement) {
+  modifyPlayButton(playButtonElement);
+  fadeHomePage();
+}
+
+function modifyPlayButton(element) {
   element.classList.remove("w3-btn");
   element.classList.add("w3-animate-zoom");
-  element.innerHTML = "WIP";
+  element.innerHTML = "(this is WIP)";
+}
+
+function fadeHomePage() {
+  fadeChildrenOf(document.getElementById("middle"));
+  fadeChildrenOf(document.getElementById("top-right"));
 }
